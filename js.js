f4334567d9fa849a8978b7121eaa9379b78281c8
Абрамов Виталий Эдуардович <V.Abramov@action-media.ru>
2019-01-24 22:43:36 +0300

    fix sw

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('sw.js')
         .then(() => { console.log('[SW] Registered!'); })
         .catch((err) => { console.log('[SW] Not registered!', err.message); });
 }
